f48c91fbe3eb171a0611936e695d72cb825ca006
devyf <hyfmailsave@163.com>
2021-04-05 02:05:33 +0800

    提交Springcloud zuul相关服务配置代码

---
+++ springcloud_hello/springcloud-zuul-gateway-9527/src/main/java/com/fengye/springcloud/Zuul_9527_StartSpringCloudApp.java
@@ -0,0 +1,13 @@
+package com.fengye.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy  //增加zuul网关代理支持
+public class Zuul_9527_StartSpringCloudApp {
+    public static void main(String[] args) {
+        SpringApplication.run(Zuul_9527_StartSpringCloudApp.class, args);
+    }
+}
