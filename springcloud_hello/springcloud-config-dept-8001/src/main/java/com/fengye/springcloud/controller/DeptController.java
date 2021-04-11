49456771385199d41a27191d737ef7cc1a2ca492
huangyifeng <hyfmailsave@163.com>
2021-04-11 17:57:52 +0800

    提交Springcloud config配置全部代码： 1.springcloud configserver实现github配置文件获取； 2.基于bootstrap.yml改造eureka客户端配置，实现配置抽取； 3.基于bootstrap.yml改造服务提供者配置，实现配置抽取

---
+++ springcloud_hello/springcloud-config-dept-8001/src/main/java/com/fengye/springcloud/controller/DeptController.java
@@ -0,0 +1,70 @@
+package com.fengye.springcloud.controller;
+
+import com.fengye.springcloud.pojo.Dept;
+import com.fengye.springcloud.service.DeptService;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/dept")
+public class DeptController {
+    @Autowired
+    private DeptService service;
+
+    @Autowired  //对外暴露一个服务发现的接口
+    private DiscoveryClient client;
+
+    //集群情况下，用于订单服务查看到底调用的是哪个商品微服务节点
+    @Value("${server.port}")
+    private String port;
+
+    @Value("${spring.datasource.url}")
+    private String dataBaseUrl;
+
+    @PostMapping("/add")
+    public boolean addDept(Dept dept){
+        return service.addDept(dept);
+    };
+
+    @GetMapping("/queryById/{id}")
+    public Dept queryById(@PathVariable("id") Long id){
+        Dept dept = service.queryById(id);
+        Dept result = new Dept();
+        BeanUtils.copyProperties(dept, result);
+        result.setDname(result.getDname() + " data from port=" + port);  //查看员工属于哪个端口
+        System.out.println(result.getDname());
+        System.out.println(dataBaseUrl);
+        return result;
+    };
+
+    @GetMapping("/queryAll")
+    public List<Dept> queryAll(){
+        return service.queryAll();
+    };
+
+    //获取注册进来的微服务的一些消息
+    @GetMapping("/discovery")
+    public Object discovery(){
+        //获取微服务列表的清单
+        List<String> services = client.getServices();
+        System.out.println("discovery=>:" + services);
+        //得到一个具体的微服务信息，通过具体的微服务Application id
+        List<ServiceInstance> instances = client.getInstances("SPRINGCLOUD-PROVIDER-DEPT");
+        for (ServiceInstance instance : instances) {
+            System.out.println(
+                    instance.getHost() + "\t" +
+                    instance.getPort() + "\t" +
+                    instance.getUri() + "\t" +
+                    instance.getServiceId()
+                            );
+        }
+
+        return this.client;
+    }
+}
