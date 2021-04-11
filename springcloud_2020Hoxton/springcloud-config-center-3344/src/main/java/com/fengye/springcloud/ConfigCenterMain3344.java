a6d9ae77e59bd5ccc04d680d4c9e1b921d952381
huangyifeng <hyfmailsave@163.com>
2021-04-11 18:48:45 +0800

    提交Springcloud H版集成Springcloud config server代码： 已经成功注册config server3344到注册中心并实现yml文件读取访问

--- springcloud_2020Hoxton/springcloud-config-center-3344/src/main/java/com/fengye/springcloud/ConfigCenterMain3344.java
+++ springcloud_2020Hoxton/springcloud-config-center-3344/src/main/java/com/fengye/springcloud/ConfigCenterMain3344.java
@@ -4,9 +4,11 @@ package com.fengye.springcloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableConfigServer
+@EnableConfigServer  //服务端需要添加激活config配置中心
+@EnableEurekaClient  //服务注册到Eureka注册中心
 public class ConfigCenterMain3344 {
     public static void main(String[] args) {
         SpringApplication.run(ConfigCenterMain3344.class, args);
