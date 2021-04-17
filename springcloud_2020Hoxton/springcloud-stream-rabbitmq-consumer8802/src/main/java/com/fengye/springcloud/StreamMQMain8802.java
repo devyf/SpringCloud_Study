8194b71b9d61bdc72a445f7030e2b11e395d612a
huangyifeng <hyfmailsave@163.com>
2021-04-17 22:04:34 +0800

    Springcloud分布式框架学习： 提交SpringCloud Stream基于RabbitMQ实现消息发送接收

---
+++ springcloud_2020Hoxton/springcloud-stream-rabbitmq-consumer8802/src/main/java/com/fengye/springcloud/StreamMQMain8802.java
@@ -0,0 +1,16 @@
+package com.fengye.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Description:
+ * @Author: huang
+ * @Date: 2021/4/17 21:03
+ */
+@SpringBootApplication
+public class StreamMQMain8802 {
+    public static void main(String[] args) {
+        SpringApplication.run(StreamMQMain8802.class, args);
+    }
+}
---
+++ springcloud_2020Hoxton/springcloud-stream-rabbitmq-provider8801/src/main/java/com/fengye/springcloud/service/IMessageProvider.java
@@ -0,0 +1,5 @@
+package com.fengye.springcloud.service;
+
+public interface IMessageProvider {
+    public String send();
+}
